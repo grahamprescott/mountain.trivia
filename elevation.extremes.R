bbb8b51b678c0f202255b8a9f610f4575ca8644b
grahamprescott <graham.prescott.research@gmail.com>
2021-12-02 14:03:00 +0100

    tidy and comment the code

--- elevation.extremes.R
+++ elevation.extremes.R
@@ -1,13 +1,19 @@
 elevation.data <- read.csv("./elevation.extremes.csv")
+library(tidyverse)
 head(elevation.data)
 ggplot(elevation.data) +
   theme_classic(18) +
   geom_point(aes(x = max, y = min)) +
-  annotate("segment", x = 2500, xend = 3400, y = 1400, yend = 1400, colour = "black", size=0.1, alpha=1, arrow=arrow())
+  annotate("segment", x = 2500, xend = 3400, y = 1400, yend = 1400, colour = "black", size=0.2, alpha=2, arrow=arrow()) +
+  annotate(geom="text", x=4634, y=250, label="CH",
+           color="red") +
+  annotate(geom="text", y=1470, x=3482, label="?",
+           color="red") +
+ labs(x = "Maximum elevation (MASL)", y = "Minimum elevation (MASL)")
 ggplot(elevation.data) +
   theme_classic(18) +
   geom_point(aes(x = max, y = min)) +
-  geom_text(aes(label=country, x = max, y = min),hjust=0, vjust=0, size = 3)
+  geom_text(aes(label=country, x = Maximum elevation, y = min),hjust=0, vjust=0, size = 3)
 ggplot(elevation.data) +
   theme_classic(18) +
   geom_point(aes(x = max-min, y = max)) +
